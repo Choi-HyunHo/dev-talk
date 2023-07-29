98c6a3cbffae283ca35bdcfbbc802fcdc0cdebf3
최현호 <x7788@naver.com>
2023-07-29 17:05:48 +0900

    feat : layout 및 header & footer

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="w-full font-mono flex flex-col justify-center items-center pt-10 pb-6 transition duration-500">
+            <div className="flex justify-center gap-4 items-center pt-4 border-t-2 w-36"></div>
+            <h2 className="text-sm mt-2">Copyright © 2023 HyunHo Choi</h2>
+            <h2 className="text-xs mt-2">Dev-TAlk</h2>
+        </footer>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <div>1</div>;
+    return <></>;
 }
