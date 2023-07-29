ba242b7c3616be6504d9fc18831fa8dcd74be92f
최현호 <x7788@naver.com>
2023-07-29 22:03:21 +0900

    feat : next-auth 를 활용한 구글 로그인

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,15 +2,30 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import Logo from "../../../public/images/logo.png";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Avatar from "./Avatar";
+import LoginBtn from "./LoginBtn";
 
 export default function Header() {
+    const { data: session } = useSession();
+
     return (
         <header className="flex items-center justify-between sticky top-0 left-0 h-20 w-full">
             <Link href="/" className="flex items-center">
                 <Image src={Logo} alt="logo" width={200} />
             </Link>
             <nav className="flex gap-4 text-lg">
-                <button>로그인 &nbsp; | &nbsp; 회원가입</button>
+                {session ? (
+                    <div className="flex items-center gap-4">
+                        <Avatar image={session.user?.image} />
+                        <LoginBtn text="로그아웃" onClick={() => signOut()} />
+                    </div>
+                ) : (
+                    <LoginBtn
+                        text="로그인 &nbsp; | &nbsp; 회원가입"
+                        onClick={() => signIn()}
+                    />
+                )}
             </nav>
         </header>
     );
